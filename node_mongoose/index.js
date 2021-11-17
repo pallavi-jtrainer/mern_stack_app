72d9f90bbacc3be8675c9a74617cce32d0f39509
pallavi-jtrainer <61450900+pallavi-jtrainer@users.noreply.github.com>
2021-11-17 16:58:39 +0530

    Add files via upload

--- node_mongoose/index.js
+++ node_mongoose/index.js
@@ -25,14 +25,49 @@ mongoose.connect(CONNECTION_URL, cb);
 
 let conn = mongoose.connection;
 
-var findRecords = (err, data) => {
-    console.log("Total Records found: " + data.length);
-    data.forEach((each) => {
-        console.log(each);
-    });
-}
-userModel.find(findRecords);
+//count documents
+userModel.count({age: {$lt: 30}}, (err, num) => {
+    console.log("Number of documents in the users Schema: " + num);
+} );
 
+// var findRecords = (err, data) => {
+//     console.log("Total Records found: " + data.length);
+//     data.forEach((each) => {
+//         console.log(each);
+//     });
+// }
+
+//update a single document
+// userModel.findByIdAndUpdate("6194d8455943b490fa456905", {age: 25}, (err, data) => {
+//     if(!err) {
+//         console.log(data);
+//     } else {
+//         console.log(err);
+//     }
+// });
+
+// userModel.updateOne({_id: mongoose.Types.ObjectId("6194d69460153091321ae488")}, {age: 30}, (err, data) => {
+//     if(!err) {
+//         console.log(data);
+//     } else {
+//         console.log(err);
+//     }
+// });
+
+//get all records
+//userModel.find(findRecords);
+
+//get single record
+// userModel.findById("6194d8455943b490fa456905", "age firstName lastName",
+//     (err, data) => {
+//         if(!err) {
+//             console.log(data);
+//         } else {
+//             console.log(err);
+//         }
+//     });
+
+//insert a document
 // user1.save((err, user1) => {
 //     if(!err) {
 //         console.log("Document Saved");
