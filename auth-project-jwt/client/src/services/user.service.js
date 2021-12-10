5442a1cedeec31b85e6d7d63399e3acc053954bc
pallavi-jtrainer <61450900+pallavi-jtrainer@users.noreply.github.com>
2021-12-10 10:15:59 +0530

    client completed

--- auth-project-jwt/client/src/services/user.service.js
+++ auth-project-jwt/client/src/services/user.service.js
@@ -1,11 +1,24 @@
-import React from 'react';
 import axios from "axios";
 import authHeader from './auth-header';
 
 const BASE_URL = "http://localhost:5000/users";
 
-class UserService extends React.Component {
-    
-}
-
-export default UserService;
+const getPublicContent = () => {
+    return axios.get(BASE_URL + "/public");
+  };
+  
+  const getUserContent = () => {
+    return axios.get(BASE_URL + "/user", { headers: authHeader() });
+  };
+  
+  
+  const getAdminContent = () => {
+    return axios.get(BASE_URL + "/admin", { headers: authHeader() });
+  };
+  
+  // eslint-disable-next-line import/no-anonymous-default-export
+  export default {
+    getPublicContent,
+    getAdminContent,
+    getUserContent
+  };
